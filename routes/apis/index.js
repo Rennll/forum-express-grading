232b25b321801e67d51e217e3b9797b12145624f
Rennll <dt616717@gmail.com>
2022-07-14 10:57:57 +0800

    feat: add api register

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -11,6 +11,7 @@ router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.post('/signup', userController.signUp)
 
 router.use('/', apiErrorHandler)
 
